df7584a7a982f73f5bef797d87d80eaf482dc9d3
Isaac <swengineerwithisaac@gmail.com>
2022-06-06 18:57:51 +0800

    Updated version 1 and 2

--- JavaScript/FizzBuzz/index.js
+++ JavaScript/FizzBuzz/index.js
@@ -1,30 +1,44 @@
 
 // Version 1
-function fizzBizz (value) {
+function fizzBuzz (value) {
     let newArray = [];
 
     for (i = 0; i < value; i++) {
         let current = i + 1;
-        if (current % 3 === 0) {
+
+        if (current % 3 === 0 && current % 5 === 0) {
+            newArray.push("FizzBuzz");
+        } else if (current % 3 === 0) {
             newArray.push("Fizz")
-            i++;
+        } else if (current % 5 === 0) {
+            newArray.push("Buzz");
+        } else {
+            newArray.push(current);
         }
-        if (current % 5 === 0) {
-            newArray.push("Bizz");
-            i++
-        }
-        if (current % 3 == 0 && current % 5 == 0) {
-            newArray.push("FizzBizz");
-            i++
-        }
-        newArray.push(i + 1);
     }
     return newArray;
 }
 
-const array = fizzBizz(100);
-console.log(fizzBizz(100));
+const array = fizzBuzz(100);
+console.log(array);
 
 
 
-// Version 3
+// Version 2
+
+let array2 = [];
+let count = 1;
+
+function fizzBizzManual() {
+    if (count % 3 === 0 && count % 5 === 0 ) {
+        array2.push("FizzBuzz");
+    } else if (count % 3 === 0 ) {
+        array2.push("Fizz");
+    } else if (count % 5 === 0) {
+        array2.push("Buzz");
+    } else {
+        array2.push(count);
+    }
+    count++;
+    console.log(array2);
+}
